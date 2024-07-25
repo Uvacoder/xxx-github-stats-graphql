e35f1ae7825a40e60eaddc8d3d3f1ca709b95018
uvacoder <uva22940coder@gmail.com>
2024-07-25 07:32:06 -0400

    Update AppConstants.ts

--- constants/AppConstants.ts
+++ constants/AppConstants.ts
@@ -2,7 +2,7 @@ const AppConstants = {
 	heading1: 'Retrieve User Data Using',
 	description: 'The data is being retrieved for the username:',
 	description2: 'To get your account stats, update the env file',
-	username: '@realstoman',
+	username: '@uvacoder',
 	pinnedItemTitle: 'Pinned Items on Github',
 };
 
